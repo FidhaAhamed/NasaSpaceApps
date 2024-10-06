add5db43f82488cfb56352a24293eccf7b3e43ef
bhanumathy <147066713+bhanulk@users.noreply.github.com>
2024-10-06 08:27:26 +0530

    commited asia

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import './home.css';  // Ensure this file styles the home component correctly
 import Globe from './globe'; // Ensure the path to the Globe component is correct
+import Asia from './Home/asia';
 import { Canvas } from '@react-three/fiber';
 
 // Home Component
 function Home() {
   return (
+    <>
     <div className="home">
       {/* Canvas for the 3D globe */}
       <Canvas style={{ height: '100vh', width: '100%' }} camera={{ position: [0, 0, 6] }}> {/* Adjusted Z position */}
         <Globe onClickRegion={(region) => console.log(`Clicked on ${region}`)} />
       </Canvas>
     </div>
+    <Asia />
+    </>
+    
   );
 }
 
